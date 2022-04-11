2144beeae486c04d85db802fc3cb657ea4332557
amtishler <79234652+amtishler@users.noreply.github.com>
2022-04-11 13:44:34 -0700

    Changed to 2D Project

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start(){
+        InventoryGrid grid = new InventoryGrid(10, 10, 10f);
+    }
+}
